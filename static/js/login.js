d58df06b2da957036088d44de5d8e89d8e7b7165
Tyler Chong <tylrchong@gmail.com>
2017-04-17 19:51:10 -1000

    Converting Pages Over From Static --> React

--- static/js/login.js
+++ static/js/login.js
@@ -2,7 +2,39 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <h1>Hello, world</h1>;
+    return (
+      <div>
+        <div className="row" style={{paddingTop: '170px', marginBottom: 0}}>
+          <div className="row">
+            <div className="col s10 offset-s1 l6 offset-l3 center">
+              <h2>Login to Laulima</h2>
+            </div>
+          </div>
+          <form className="col s10 offset-s1 l6 offset-l3" style={{backgroundColor: '#f7f7f7', border: '1px solid #000', paddingTop: '12px'}} name="login" id="login" action="" method="post">
+            <div className="row" style={{borderRadius: '5px', border: '1px solid #000'}}>
+              <div className="input-field col s12">
+                <i className="material-icons prefix">account_circle</i>
+                <input name="user_name" id="user_name" type="text" className="validate" />
+                <label htmlFor="user_name">User Name</label>
+              </div>
+            </div>
+            <div className="row" style={{borderRadius: '5px', border: '1px solid #000'}}>
+              <div className="input-field col s12">
+                <i className="material-icons prefix">lock</i>
+                <input name="password" id="password" type="password" className="validate" />
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+            <div id="result"></div>
+            <div className="row">
+              <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                <i className="material-icons right">send</i>
+              </button>
+           </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
--- static/js/root.js
+++ static/js/root.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 class Root extends React.Component {
   render() {
-    return <h1>Hello, world</h1>;
+    return (
+      <center>
+        <div className="row teal accent-3">
+          <h1 className="white-text" style={{margin: 0, padding: '10px 0 10px 0'}}> </h1>
+        </div>
+        <div className="row">
+          <div className="sections">
+          </div>
+          {/* {this.props.body.sections.map(function(e, i) {
+            return(
+              <div className="col s10 offset-s1 l10 offset-l1">
+                <div className="card-panel">
+                  <h4 style={{borderBottom: '3px solid #333'}}>{ section.keys()[0] }</h4>
+                  <iframe src="{{section.values()[0]['src']}}" frameborder="0" scrolling="no" onload="resizeIframe(this)"></iframe>
+                </div>
+              </div>
+            )
+          })
+          } */}
+        </div>
+      </center>
+    );
   }
 }
 
